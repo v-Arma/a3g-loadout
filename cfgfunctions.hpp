bed8e5ce76492a7491854bf50e4251b1361f6992
Cephei <cepheidev+github@gmail.com>
2014-09-29 16:24:54 +0200

    Small formatting fix

--- cfgfunctions.hpp
+++ cfgfunctions.hpp
@@ -2,7 +2,7 @@ class A3G {
 	tag = "a3g";
 	class functions {
 		file = "a3g-loadout";
-		class loadout { 
+		class loadout {
 			description = "Reads and applies CfgLoadout based loadout information";
 			postInit = 1;
 		};
