83ba5184af70150de09fc0d72acea9ef4863bcbd
Cephei <cepheidev+github@gmail.com>
2015-01-02 09:26:28 +0100

    Jesus christ

--- CfgFunctions.hpp
+++ CfgFunctions.hpp
@@ -1,5 +1,5 @@
-class A3G {
-	class A3G_Loadout {
+class A3G_Loadout {
+	class Functions {
 		file = "a3g-loadout\functions";
 		class ApplyLoadout { 
 			description = "Reads and instructs the loadout function based on config entries.";
